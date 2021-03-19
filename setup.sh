de3cacc6a1a066954772426b85e5d8ac5e23d9ed
marcolussetti <git@marcolussetti.com>
2021-03-18 17:14:20 -0700

    Fixes to i3config, status, add eq10q presets

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ln -s $PWD/.config/nvim/init.vim ~/.config.nvim/init.vim
+ln -s $PWD/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $PWD/.screenlayout ~/.screenlayout
 ln -s $PWD/.i3 ~/.i3
 ln -s $PWD/eq10q_presets ~/eq10q_presets
