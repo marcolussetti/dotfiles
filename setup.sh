de54760285dea244e5c95f15be29f7e945dc58a3
marcolussetti <git@marcolussetti.com>
2021-03-20 23:09:53 -0700

    Add i3status

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -s $PWD/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $PWD/.screenlayout ~/.screenlayout
 ln -s $PWD/.i3 ~/.i3
 ln -s $PWD/eq10q_presets ~/eq10q_presets
+ln -s $PWD/.config/i3status ~/.config/i3status
