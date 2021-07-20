fe6d42d52480e430d1f48ba1748d983777b85b62
marcolussetti <git@marcolussetti.com>
2021-07-19 22:41:14 -0700

    triple screen layout

---
+++ .screenlayout/screens_triple.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output DP-0 --primary --mode 2560x1440 --pos 2560x1080 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
