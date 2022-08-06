0c9aa482bdf993be9315bddd8ccfc6bfc69def6c
marcolussetti <git@marcolussetti.com>
2022-08-05 19:03:53 -0700

    improve plex permissions script

--- misc_scripts/fix-plex-permissions.sh
+++ misc_scripts/fix-plex-permissions.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 # Set permissions for plex
-sudo chown -R marco:sharedrives /mnt/toshiba_14tb_1/v
-sudo chmod -R 777 /mnt/toshiba_14tb_1/v
+chown -R marco:sharedrives /mnt/toshiba_14tb_1/v
+chmod -R 777 /mnt/toshiba_14tb_1/v
+chown -R marco:sharedrives /mnt/x300_8tb/b/videocourses
+chown 777 /mnt/x300_8tb/b
+chown -R 777 /mnt/x300_8tb/b/videocourses
+systemctl restart plexmediaserver
