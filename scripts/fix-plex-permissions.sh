9723d74ca5902c064af1768fb343d9f997d54c88
marcolussetti <git@marcolussetti.com>
2023-05-02 20:51:27 -0700

    refactor plex

--- scripts/fix-plex-permissions.sh
+++ scripts/fix-plex-permissions.sh
@@ -2,7 +2,6 @@
 # Set permissions for plex
 chown -R marco:sharedrives /mnt/toshiba_14tb_1/v
 chmod -R 777 /mnt/toshiba_14tb_1/v
-chown -R marco:sharedrives /mnt/x300_8tb/b/videocourses
-chown 777 /mnt/x300_8tb/b
-chown -R 777 /mnt/x300_8tb/b/videocourses
+chown -R marco:sharedrives /mnt/seagate_18tb/v
+chmod -R 777 /mnt/seagate_18tb/v
 systemctl restart plexmediaserver
