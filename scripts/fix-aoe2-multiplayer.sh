d2a9eb0a0251ed6079c7bbd039777aa3a35bf3bf
marcolussetti <git@marcolussetti.com>
2025-01-19 11:05:34 -0800

    fix-aoe2-multiplayer: add support for second drive

--- scripts/fix-aoe2-multiplayer.sh
+++ scripts/fix-aoe2-multiplayer.sh
@@ -3,5 +3,9 @@ cd "$(mktemp -d)"
 wget "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe"
 cabextract vc_redist.x64.exe
 cabextract a10
+
 chmod u+w ~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ucrtbase.dll
 yes | cp ucrtbase.dll ~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32
+
+#chmod u+w /mnt/seagate_18tb/SteamLibrary/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ucrtbase.dll
+#yes | cp ucrtbase.dll /mnt/seagate_18tb/SteamLibrary/steamapps/compatdata/813780/pfx/drive_c/windows/system32
